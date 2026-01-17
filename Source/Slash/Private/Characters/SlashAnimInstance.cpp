3bdc6e4170f2571ac71850957451c5398b234380
dongge-c-language <2296954740@qq.com>
2026-01-18 00:02:03 +0800

    The Anim Instance（动画实例）
    动画蓝图对应的 C++ 类

---
+++ Source/Slash/Private/Characters/SlashAnimInstance.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/SlashAnimInstance.h"
+#include "Characters/SlashCharacter.h"  //要使用SlashCharacter，就要引入它的头文件和类路径。
+#include "GameFramework/CharacterMovementComponent.h"//移动组件
+#include "Kismet/KismetMathLibrary.h"//使用Kismet数学库的静态函数 
+
+void USlashAnimInstance::NativeInitializeAnimation()
+{
+	
+	Super::NativeInitializeAnimation(); //因为是重写函数，所以调用父版本
+	
+	//TryGetPawnOwner();//返回pawn。进行类型转换
+	SlashCharacter=Cast<ASlashCharacter>(TryGetPawnOwner());//获取tryGetPawnOwner()的返回值，尝试将其强转为SlashCharacter。这样初始化SlashCharacter
+	if (SlashCharacter)
+	{
+		//用这个返回的角色移动组件来初始化我们的GetCharacterMovement变量。
+		SlashCharacterMovement = SlashCharacter->GetCharacterMovement();
+	}
+}
+
+void USlashAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	//UKismetMathLibrary这个类的函数都是静态的，不用创建Kismet数学库类型的对象，这些函数不改变任何状态和结果，只是计算和返回结果
+	//检查这个指针，确保只在非空时访问
+	if (SlashCharacterMovement)
+	{
+		//获取SlashCharacterMovement在xy方向上的速度
+		GroundSpeed = UKismetMathLibrary::VSizeXY(SlashCharacterMovement->Velocity);
+	}
+}
---
+++ Source/Slash/Public/Characters/SlashAnimInstance.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SlashAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLASH_API USlashAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	virtual void NativeInitializeAnimation() override;  //重写函数，继承自animinstance类。重写蓝图事件类表的函数。
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	class ASlashCharacter* SlashCharacter;//创建一个变量来初始化变量。
+
+	UPROPERTY(BlueprintReadOnly,Category = Movement)
+	class UCharacterMovementComponent* SlashCharacterMovement;//创建移动组件
+
+	//创建地面速度，归类到移动
+	UPROPERTY(BlueprintReadOnly,Category=Movement)
+	float GroundSpeed;
+};
