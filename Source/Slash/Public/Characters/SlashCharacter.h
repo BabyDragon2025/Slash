2f69fae57b03122b3130cfa09ece3a36b5712fd5
dongge-c-language <2296954740@qq.com>
2026-02-19 06:14:59 +0800

    Echo 的生命值（Echo's Health）
    ——对 Echo 造成伤害

--- Source/Slash/Public/Characters/SlashCharacter.h
+++ Source/Slash/Public/Characters/SlashCharacter.h
@@ -24,6 +24,7 @@ public:
 	ASlashCharacter();
 	//virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
 	virtual void GetHit_Implementation(const FVector& ImpactPoint, AActor* Hitter) override;//GetHit_Implementation覆盖了GitHit，因为GitHit设置成了原生蓝图，不再是虚函数。
 
 
