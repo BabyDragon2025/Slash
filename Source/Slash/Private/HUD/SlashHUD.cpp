f9ecd3f1c125415ed8a94bec330586fb3dd40e9b
dongge-c-language <2296954740@qq.com>
2026-02-20 13:19:48 +0800

    劈砍（ HUDSlash HUD）
     ——为游戏 HUD 创建 C++ 类

---
+++ Source/Slash/Private/HUD/SlashHUD.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HUD/SlashHUD.h"
+#include "HUD/SlashOverlay.h"
+
+void ASlashHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//创建HUD小部件并添加到视口中
+	UWorld* World = GetWorld();
+	if (World)
+	{
+		APlayerController* Controller = World->GetFirstPlayerController();
+		if (Controller && SlashOverlayClass)
+		{
+			USlashOverlay* SlashOverlay = CreateWidget<USlashOverlay>(Controller, SlashOverlayClass);
+			SlashOverlay->AddToViewport();
+		}
+	}
+}
